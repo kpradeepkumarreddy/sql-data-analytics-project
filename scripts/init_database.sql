14c2dcd84a10e15bd9d4206d25361f0d7d04e32a
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-30 05:25:53 +0530

    Create and initialize datawarehouse_analytics database
    This script creates a new database named 'datawarehouse_analytics', drops it if it exists, and sets up the necessary tables for customers, products, and sales data.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,63 @@
+/*
+=============================================================
+			Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, this script creates the tables and loads data into it.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+DROP DATABASE IF EXISTS datawarehouse_analytics;
+CREATE DATABASE datawarehouse_analytics;
+
+USE datawarehouse_analytics;
+
+DROP TABLE IF EXISTS dim_customers;
+CREATE TABLE dim_customers(
+	customer_key int,
+	customer_id int,
+	customer_number varchar(50),
+	first_name varchar(50),
+	last_name varchar(50),
+	country varchar(50),
+	marital_status varchar(50),
+	gender varchar(50),
+	birthdate date,
+	create_date date
+);
+
+
+DROP TABLE IF EXISTS dim_products;
+CREATE TABLE dim_products(
+	product_key int ,
+	product_id int ,
+	product_number varchar(50) ,
+	product_name varchar(50) ,
+	category_id varchar(50) ,
+	category varchar(50) ,
+	subcategory varchar(50) ,
+	maintenance varchar(50) ,
+	cost int,
+	product_line varchar(50),
+	start_date date 
+);
+
+
+DROP TABLE IF EXISTS fact_sales;
+CREATE TABLE fact_sales(
+	order_number varchar(50),
+	product_key int,
+	customer_key int,
+	order_date date,
+	shipping_date date,
+	due_date date,
+	sales_amount int,
+	quantity tinyint,
+	price int 
+);
 
