3f189d913168317b84f392a8498ab9c1c5631df2
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-30 17:03:58 +0530

    Add measures exploration SQL script for key metrics
    This SQL script is designed for exploring key metrics related to sales, orders, products, and customers. It includes placeholders for calculating totals, averages, and counts to provide insights into business performance.

---
+++ scripts/measures_exploration.sql
@@ -0,0 +1,34 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
+
+-- Find the Total Sales
+
+
+-- Find how many items are sold
+
+
+-- Find the average selling price
+
+
+-- Find the Total number of Orders
+
+
+-- Find the total number of products
+
+-- Find the total number of customers
+
+
+-- Find the total number of customers that has placed an order
+
+
+-- Generate a Report that shows all key metrics of the business
