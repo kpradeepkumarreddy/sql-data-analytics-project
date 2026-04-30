30bbdc7f62676b395a6dee1f075b7287a429f845
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-30 17:08:00 +0530

    Create ranking_analysis.sql for performance metrics
    Added SQL script for ranking analysis of products and customers.

---
+++ scripts/ranking_analysis.sql
@@ -0,0 +1,39 @@
+/*
+===============================================================================
+Ranking Analysis
+===============================================================================
+Purpose:
+    - To rank items (e.g., products, customers) based on performance or other metrics.
+    - To identify top performers or laggards.
+
+SQL Functions Used:
+    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
+    - Clauses: GROUP BY, ORDER BY
+===============================================================================
+*/
+
+-- Which 5 products Generating the Highest Revenue?
+-- Simple Ranking
+
+
+
+
+
+-- Complex but Flexibly Ranking Using Window Functions
+
+
+
+-- What are the 5 worst-performing products in terms of sales?
+
+
+
+
+-- Find the top 10 customers who have generated the highest revenue
+
+
+
+
+-- The 3 customers with the fewest orders placed
+
+
+
