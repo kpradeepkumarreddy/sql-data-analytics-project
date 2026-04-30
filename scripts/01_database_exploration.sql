eab5907bf846a58b2f427653a5fb0a98904e0384
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-30 18:46:19 +0530

    Add SQL queries to explore database tables and columns

--- scripts/01_database_exploration.sql
+++ scripts/01_database_exploration.sql
@@ -11,3 +11,13 @@ Table Used:
     - INFORMATION_SCHEMA.COLUMNS
 ===============================================================================
 */
+
+-- Retrieve a list of all tables in the database
+SELECT * FROM INFORMATION_SCHEMA.TABLES;
+
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME LIKE 'dim%' OR TABLE_NAME LIKE 'fact%';
+
+
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'dim_customers';
