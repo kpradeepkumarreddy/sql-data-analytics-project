b26c5f8c03e868ae3d96c970aa1fd011548db30a
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-30 17:02:01 +0530

    Create date_range_exploration.sql for data analysis
    Add SQL script for date range exploration and analysis.

---
+++ scripts/date_range_exploration.sql
@@ -0,0 +1,20 @@
+/*
+===============================================================================
+Date Range Exploration 
+===============================================================================
+Purpose:
+    - To determine the temporal boundaries of key data points.
+    - To understand the range of historical data.
+
+SQL Functions Used:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- Determine the first and last order date and the total duration in months
+
+
+
+
+
+-- Find the youngest and oldest customer based on birthdate
