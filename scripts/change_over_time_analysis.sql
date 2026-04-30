ced4d659d2b0d7b487ff74b78a85612bf0343491
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-30 17:09:48 +0530

    Create change_over_time_analysis.sql for time analysis
    Added SQL script for analyzing changes over time in key metrics.

---
+++ scripts/change_over_time_analysis.sql
@@ -0,0 +1,28 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+
+-- Analyse sales performance over time
+-- Quick Date Functions
+
+
+
+
+-- DATETRUNC()
+
+
+
+
+
+-- FORMAT()
