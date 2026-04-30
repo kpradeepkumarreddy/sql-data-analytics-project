8ce562ddf392342950baf3253c05cedaa19ce8ce
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-04-30 17:01:07 +0530

    Add dimensions_exploration.sql for table structure analysis
    Initial creation of dimensions exploration SQL script for analyzing dimension tables.

---
+++ scripts/dimensions_exploration.sql
@@ -0,0 +1,17 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+
+
+-- Retrieve a list of unique categories, subcategories, and products
