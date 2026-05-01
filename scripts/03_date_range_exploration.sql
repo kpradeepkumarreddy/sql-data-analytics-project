f684fb2dab73a14b9b30d9c8a668b95d0f8df246
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-05-01 06:16:57 +0530

    Replace DATEDIFF with TIMESTAMPDIFF in SQL queries

--- scripts/03_date_range_exploration.sql
+++ scripts/03_date_range_exploration.sql
@@ -7,14 +7,30 @@ Purpose:
     - To understand the range of historical data.
 
 SQL Functions Used:
-    - MIN(), MAX(), DATEDIFF()
+    - MIN(), MAX(), TIMESTAMPDIFF()
 ===============================================================================
 */
 
+
 -- Determine the first and last order date and the total duration in months
-
-
+SELECT 
+	MIN(order_date) AS first_order_date,
+	MAX(order_date) AS last_order_date,
+	TIMESTAMPDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_date_range
+FROM datawarehouse_analytics.fact_sales;
 
 
 
 -- Find the youngest and oldest customer based on birthdate
+SELECT
+	TIMESTAMPDIFF(YEAR, MAX(order_date), CURDATE()) AS youngest_customer,
+	TIMESTAMPDIFF(YEAR, MIN(order_date), CURDATE()) AS oldest_customer
+FROM datawarehouse_analytics.fact_sales;
+
+
+
+-- help TIMESTAMPDIFF;
+-- help now;
+-- help curdate;
+-- help datediff;
+-- SELECT CURDATE();
