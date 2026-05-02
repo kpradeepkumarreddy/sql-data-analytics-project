df04abf21aee9f98d9f3516e05feda63b23b9b6b
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-05-02 11:15:42 +0530

    Update SQL queries for sales analysis with DATE_FORMAT

--- scripts/07_change_over_time_analysis.sql
+++ scripts/07_change_over_time_analysis.sql
@@ -8,21 +8,58 @@ Purpose:
     - To measure growth or decline over specific periods.
 
 SQL Functions Used:
-    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
-    - Aggregate Functions: SUM(), COUNT(), AVG()
+    - Date Functions: YEAR(), MONTH(), DATE_FORMAT()
+    - Aggregate Functions: SUM()
 ===============================================================================
 */
 
+USE datawarehouse_analytics;
+
 -- Analyse sales performance over time
 -- Quick Date Functions
+select YEAR(order_date) AS sales_year, SUM(sales_amount) AS total_sales from fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY sales_year
+ORDER BY sales_year;
+
+
+select YEAR(order_date) AS sales_year, MONTH(order_date) AS sales_month, SUM(sales_amount) AS total_sales from fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY sales_year, sales_month
+ORDER BY sales_year, sales_month;
 
 
 
+-- truncate to month
+-- DATE_FORMAT(order_date, '%Y-%m-01')
 
--- DATETRUNC()
+select DATE_FORMAT(order_date, '%Y-%m-01') AS sale_month, SUM(sales_amount) AS total_sales from fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY sale_month
+ORDER BY sale_month;
+
+
+-- truncate to year
+-- DATE_FORMAT(order_date, '%Y-01-01')
+
+select DATE_FORMAT(order_date, '%Y-01-01') AS sale_year, SUM(sales_amount) AS total_sales from fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY sale_year
+ORDER BY sale_year;
 
 
 
+help date_format;
+
+-- %Y - 4-digit year
+-- %y - 2-digit year
+-- %m - month number
+-- %b - short month name
+-- %M - full month name
+-- %d - day
+
+select DATE_FORMAT(order_date, '%Y-%b') AS sale_month, SUM(sales_amount) AS total_sales from fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY sale_month
 
 
--- FORMAT()
