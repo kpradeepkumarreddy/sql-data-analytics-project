c393d4cdf9737667a8024dc733b9592c11c2ed63
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-05-02 12:53:08 +0530

    Add cumulative sales analysis SQL queries

--- scripts/08_cumulative_analysis.sql
+++ scripts/08_cumulative_analysis.sql
@@ -13,6 +13,57 @@ SQL Functions Used:
 */
 
 
--- Calculate the total sales per month 
+USE datawarehouse_analytics;
+
+
+-- Calculate the total sales per year 
 -- and the running total of sales over time 
+SELECT 
+	sales_year,
+	total_sales,
+	SUM(total_sales) OVER(ORDER BY sales_year) AS running_total_sales
+FROM	
+(
+	SELECT 
+		YEAR(order_date) AS sales_year, 
+		SUM(sales_amount) AS total_sales 
+	FROM fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY sales_year
+)t;
+
+
+-- Calculate the total sales per year per month 
+-- and the running total of sales over time 
+SELECT 
+	sale_year_month,
+	total_sales,
+	SUM(total_sales) OVER(PARTITION BY YEAR(sale_year_month) ORDER BY sale_year_month) AS running_total_sales
+FROM	
+(
+	SELECT 
+		DATE_FORMAT(order_date, '%Y-%m-01') AS sale_year_month, 
+		SUM(sales_amount) AS total_sales 
+	FROM fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY sale_year_month
+)t;
+
+
 -- and moving average price
+SELECT 
+	sales_year,
+	total_sales,
+	SUM(total_sales) OVER(ORDER BY sales_year) AS running_total_sales,
+	average_price,
+	ROUND(AVG(average_price) OVER(ORDER BY sales_year), 2) AS moving_average_price
+FROM	
+(
+	SELECT 
+		YEAR(order_date) AS sales_year, 
+		SUM(sales_amount) AS total_sales,
+		ROUND(AVG(price), 2) AS average_price
+	FROM fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY sales_year
+)t;
