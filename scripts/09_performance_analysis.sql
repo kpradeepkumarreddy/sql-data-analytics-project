df6488e6485bc92b9fad30b962aa982885ec5129
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-05-02 19:41:35 +0530

    Add yearly performance analysis for product sales
    This SQL script analyzes yearly product performance by comparing current sales to average sales and previous year's sales, providing insights on sales trends.

--- scripts/09_performance_analysis.sql
+++ scripts/09_performance_analysis.sql
@@ -14,5 +14,42 @@ SQL Functions Used:
 ===============================================================================
 */
 
-/* Analyze the yearly performance of products by comparing their sales 
-to both the average sales performance of the product and the previous year's sales */
+
+USE datawarehouse_analytics;
+
+/* 
+ 	Analyze the yearly performance of products by comparing their sales 
+	to both the average sales performance of the product and the previous year's sales 
+*/
+
+WITH yearly_sales AS
+(
+	SELECT 
+		dp.product_name,
+		YEAR(fs.order_date) AS order_year,
+		SUM(fs.sales_amount) AS current_sales
+	FROM dim_products AS dp LEFT JOIN fact_sales fs ON dp.product_key = fs.product_key
+	WHERE fs.order_date IS NOT NULL
+	GROUP BY dp.product_name, order_year
+	ORDER BY dp.product_name
+)
+SELECT 
+	product_name,
+	order_year,
+	current_sales,
+	ROUND(AVG(current_sales) OVER(PARTITION BY product_name), 2) AS avg_sales,
+	current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name), 2) AS avg_sales_diff,
+	CASE 
+		WHEN current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name), 2) < 0 THEN 'Below Average'
+		WHEN current_sales - ROUND(AVG(current_sales) OVER(PARTITION BY product_name), 2) > 0 THEN 'Above Average'
+		ELSE 'Average'
+	END AS sales_performance,
+	-- year-over-year analysis
+	LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS prev_year_sales,
+	current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS prev_sales_diff,
+	CASE 
+		WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decreased'
+		WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increased'
+		ELSE 'Same'
+	END AS yoy_sales_performance
+FROM yearly_sales;
