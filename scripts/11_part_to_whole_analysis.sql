32b5072864e1f6cb1dd0604a739875676df65b32
K Pradeep Kumar Reddy <kpradeepkumarreddy@gmail.com>
2026-05-03 09:06:04 +0530

    Add SQL query for category revenue analysis

--- scripts/11_part_to_whole_analysis.sql
+++ scripts/11_part_to_whole_analysis.sql
@@ -13,4 +13,21 @@ SQL Functions Used:
 ===============================================================================
 */
 
+USE datawarehouse_analytics;
+
 -- Which categories contribute the most to overall sales?
+WITH product_category_revenues AS 
+(
+	SELECT 
+		dp.category, 
+		SUM(fs.sales_amount) AS category_revenue 
+	FROM fact_sales fs LEFT JOIN dim_products dp ON dp.product_key=fs.product_key
+	GROUP BY dp.category
+	ORDER BY category_revenue DESC
+)
+SELECT 
+	category,
+	category_revenue,
+	ROUND((category_revenue/SUM(category_revenue) OVER())*100, 2) AS revenue_contribution
+FROM product_category_revenues;
+
